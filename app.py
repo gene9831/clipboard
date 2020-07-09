e1247dd5a73ee86faa1b81a940d37821cbbd9833
gene9831 <pacify.98@gmail.com>
2020-07-10 01:11:29 +0800

    normal update

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import uuid
 import os
 import time
 from threading import Timer, Semaphore
-from enum import IntEnum
+from enum import Enum
+import json
 
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -15,29 +16,65 @@ app.secret_key = os.urandom(16)
 
 
 sem = Semaphore()
-detect_sec = 1
+polling_time = 0.5
 
-clipboard = {'clipboard': ''}
+clipboard = ''
 online_users = {}
 lock_owner = None
 
 
-class RespStatus(IntEnum):
+class RespStatus(Enum):
     SUCCESS = 0
     FAIL = 1
     ERROR = 2
 
 
-@app.route('/')
-def home():
-    session['user'] = str(uuid.uuid4())
-    return render_template('index.html')
+class RespData():
+    def __init__(self, status, **kwargs):
+        self._status = status
+        self._message = status.name.lower()
+        self.data = kwargs
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, _status):
+        self._status = _status
+        self._message = _status.name.lower()
+
+    def add_data(self, **kwargs):
+        for k, v in kwargs.items():
+            self.data[k] = v
+
+    def to_json(self):
+        res = {
+            'status': self._status.value,
+            'message': self._message
+        }
+        for k, v in self.data.items():
+            if k == 'status' or k == 'message':
+                continue
+
+            if isinstance(v, str) or can_convert2json(v):
+                res[k] = v
+
+        return json.dumps(res)
 
 
-def is_online(user):
+def can_convert2json(obj):
+    try:
+        json.dumps(obj)
+    except ValueError:
+        return False
+    return True
+
+
+def heartbeat_detect(user):
     if user not in online_users:
         return
-    if time.time() - online_users[user] > detect_sec:
+    if time.time() - online_users[user] > polling_time * 2:
         online_users.pop(user)
 
         global lock_owner
@@ -47,69 +84,78 @@ def is_online(user):
             sem.release()
 
 
+@app.route('/')
+def home():
+    session['user'] = str(uuid.uuid4())
+    return render_template('index.html')
+
+
 @app.route('/heartbeat', methods=['POST'])
 def heartbeat():
     if 'user' in session:
         online_users[session['user']] = time.time()
-        Timer(detect_sec, is_online, (session['user'],)).start()
-        return {'user': session['user']}
-    return {'user': None}
+        Timer(polling_time * 2, heartbeat_detect, (session['user'],)).start()
+
+        return RespData(RespStatus.SUCCESS,
+                        clipboard=clipboard,
+                        lock_owner=lock_owner,
+                        online_users_num=len(online_users)).to_json()
+
+    return RespData(RespStatus.ERROR).to_json()
 
 
 @app.route('/acquire_lock')
 def acquire_lock():
-    res = {'status': RespStatus.FAIL}
+    res = RespData(RespStatus.FAIL)
     sem.acquire()
     global lock_owner
     if lock_owner is None and 'user' in session:
         lock_owner = session['user']
-        res['status'] = RespStatus.SUCCESS
+        res.status = RespStatus.SUCCESS
     sem.release()
     if 'user' not in session:
-        res['status'] = RespStatus.ERROR
-    return res
+        res.status = RespStatus.ERROR
+    return res.to_json()
 
 
 @app.route('/release_lock')
 def release_lock():
-    res = {'status': RespStatus.FAIL}
+    res = RespData(RespStatus.FAIL)
     sem.acquire()
     global lock_owner
     if lock_owner is not None and 'user' in session and lock_owner == session['user']:
         lock_owner = None
-        res = {'status': RespStatus.SUCCESS}
+        res.status = RespStatus.SUCCESS
     sem.release()
     if 'user' not in session:
-        res['status'] = RespStatus.ERROR
-    return res
+        res.status = RespStatus.ERROR
+    return res.to_json()
 
 
 @app.route('/clipboard', methods=['GET', 'POST'])
 def clipboard_content():
+    global clipboard
     if request.method == 'GET':
-        return clipboard
+        return RespData(RespStatus.SUCCESS, clipboard=clipboard).to_json()
     else:
         if 'user' in session and session['user'] == lock_owner:
-            clipboard['clipboard'] = request.form['data']
-            status = RespStatus.SUCCESS
+            clipboard = request.form['data']
+            return RespData(RespStatus.SUCCESS, clipboard=clipboard).to_json()
         else:
-            status = RespStatus.FAIL
-        return {
-            'status': status,
-            'clipboard': clipboard['clipboard'],
-        }
+            return RespData(RespStatus.FAIL).to_json()
 
 
 @app.route('/real_time_data')
 def real_time_data():
-    return {
-        'clipboard': clipboard['clipboard'],
-        'lock_owner': lock_owner,
-        'online_users_num': len(online_users)
-    }
+    res = RespData(RespStatus.SUCCESS)
+    res.add_data(clipboard=clipboard,
+                 lock_owner=lock_owner,
+                 online_users_num=len(online_users),
+                 online_users=online_users)
+    return res.to_json()
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=8899)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8899)
+    app.run(host="0.0.0.0", port=8899)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8899)
--- static/clipboard.js
+++ static/clipboard.js
@@ -46,7 +46,9 @@ var vm = new Vue({
   watch: {
     clipboard: function () {
       this.upToDate = false;
-      this.putClipboard();
+      if (this.lockStatus) {
+        this.putClipboard();
+      }
     },
   },
   methods: {
@@ -68,7 +70,6 @@ var vm = new Vue({
       center = center || false;
       duration = duration || 2000;
       customClass = customClass || "message";
-
       this.$message({
         message: message,
         type: type,
@@ -94,55 +95,47 @@ var vm = new Vue({
         .then((response) => {
           // console.log(response);
           if (response.data.status == RespStatus.success) {
+            // 剪切板最后上传时间再过500ms，更新状态
+            if (updateInterval) {
+              clearInterval(updateInterval);
+            }
+            updateInterval = setTimeout(() => {
+              this.upToDate = true;
+            }, 500);
           } else {
-          }
-
-          if (updateInterval) {
-            clearInterval(updateInterval);
-          }
-          updateInterval = setTimeout(() => {
-            this.upToDate = true;
-          }, 500);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getRealTimeData() {
-      axios
-        .get("./real_time_data")
-        .then((response) => {
-          this.lock_owner = response.data.lock_owner;
-          this.online_users_num = response.data.online_users_num;
-          if (!this.lockStatus) {
-            this.clipboard = response.data.clipboard;
-            this.upToDate = true;
+            console.log("upload clipboard failed");
           }
         })
         .catch((error) => {
           console.log(error);
         });
     },
-    alive() {
+    heartbeat() {
       axios
         .post("./heartbeat")
         .then((response) => {
-          // console.log(response);
+          this.lock_owner = response.data.lock_owner;
+          this.online_users_num = response.data.online_users_num;
+          // 未获得锁，就会刷新剪切板
+          if (!this.lockStatus) {
+            if (!this.upToDate) {
+              this.upToDate = true;
+            }
+
+            this.clipboard = response.data.clipboard;
+          }
         })
         .catch((error) => {
           console.log(error);
         });
     },
     polling() {
-      this.alive();
-      this.getRealTimeData();
+      this.heartbeat();
       setInterval(() => {
-        this.alive();
-        this.getRealTimeData();
+        this.heartbeat();
       }, 500);
     },
     acquireLock(event) {
-      let flag_blur = true;
       axios
         .get("./acquire_lock")
         .then((response) => {
@@ -154,32 +147,31 @@ var vm = new Vue({
           } else if (response.data.status == RespStatus.fail) {
             console.log("acquire lock failed");
             this.topMessage("有人在编辑哦", "warning");
+            event.srcElement.blur();
           } else {
             console.log("acquire lock error");
             this.topMessage("发生错误，请刷新网页", "error");
+            event.srcElement.blur();
           }
         })
         .catch((error) => {
           console.log(error);
           this.topMessage("似乎是网络错误", "error");
-        })
-        .then(() => {
-          if (flag_blur) {
-            event.srcElement.blur();
-          }
+          event.srcElement.blur();
         });
     },
     releaseLock() {
+      if (!this.lockStatus) {
+        return;
+      }
       axios
         .get("./release_lock")
         .then((response) => {
           if (response.data.status == RespStatus.success) {
             console.log("release lock successed");
-            if (this.lockStatus == true) {
-              // lock_owner需要通过轮询更新，这里提前清空lock_owner。
-              // 自己释放锁后就不会出现“其他人正在编辑”的情况
-              this.lock_owner = "";
-            }
+            // lock_owner需要通过轮询更新，这里提前清空lock_owner。
+            // 自己释放锁后就不会出现“其他人正在编辑”的情况
+            this.lock_owner = "";
             this.lockStatus = false;
           } else {
             console.log("release lock failed");
